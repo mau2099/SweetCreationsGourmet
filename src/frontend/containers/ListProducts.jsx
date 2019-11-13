9d054ef8bf1764561646398eb5952d8ff8986206
Alexis Araujo <ala1289@gmail.com>
2019-11-13 09:22:16 -0600

    Se describe las propiedas de los productos

--- src/frontend/containers/ListProducts.jsx
+++ src/frontend/containers/ListProducts.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import "../assets/styles/App.scss";
-import Filter from "../components/Filter";
+import React from 'react';
+import '../assets/styles/App.scss';
+import Filter from '../components/Filter';
 
 const ListProducts = () => {
   // eslint-disable-next-line no-unused-expressions
   return (
     <>
+      {/* id, image, title, description, price */}
       <h2>Pantalla de Listado de productos</h2>
       <Filter />
     </>
