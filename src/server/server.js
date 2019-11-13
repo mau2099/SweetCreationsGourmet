35c2c4fffadfbf4a2162b7447a8b02cf5a49ee6b
Alexis Araujo <ala1289@gmail.com>
2019-11-13 08:18:49 -0600

    Conexion de api login y signup

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
 import helmet from 'helmet';
+import axios from 'axios';
+import passport from 'passport';
+import boom from '@hapi/boom';
+import cookieParser from 'cookie-parser';
 import main from './routes/main';
 const authApi = require('./routes/auth');
 const notFoundHandler = require('./util/middleware/notFoundHandler');
@@ -9,13 +13,19 @@ const notFoundHandler = require('./util/middleware/notFoundHandler');
 dotenv.config();
 
 const ENV = process.env.NODE_ENV;
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-authApi(app);
+app.use(express.json());
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(`${__dirname}/public`));
 // Catch 404
 //app.use(notFoundHandler);
+
+// Basic strategy
+require('./utils/auth/strategies/basic');
 
 if (ENV === 'development') {
   console.log('Loading dev config');
@@ -39,6 +49,50 @@ if (ENV === 'development') {
   app.use(helmet.permittedCrossDomainPolicies());
   app.disable('x-powered-by');
 }
+
+app.post('/auth/sign-in', async (req, res, next) => {
+  passport.authenticate('basic', async (error, data) => {
+    try {
+      if (error || !data) {
+        return next(boom.unauthorized());
+      }
+
+      req.login(data, { session: false }, async error => {
+        if (error) {
+          next(error);
+        }
+
+        const { token, ...user } = data;
+
+        res.cookie('token', token, {
+          httpOnly: !(ENV === 'development'),
+          secure: !(ENV === 'development'),
+        });
+        res.status(200).json(user.user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post('/auth/sign-up', async (req, res, next) => {
+  const { body: user } = req;
+  try {
+    const userData = await axios({
+      url: `${process.env.API_URL}/api/auth/sign-up`,
+      method: 'post',
+      data: user,
+    });
+    res.status(201).json({
+      name: req.body.name,
+      email: req.body.email,
+      id: userData.data.data,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.get('*', main);
 
